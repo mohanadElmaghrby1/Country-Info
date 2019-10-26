ab58bc99f8b4f1e2558e66e6b12d7a7c1569fad4
mohannad <mohanad2021996@gmail.com>
2019-10-26 08:26:01 +0200

    Create CountryService

--- src/main/java/com/mohannad/countrycode/service/CountryService.java
+++ src/main/java/com/mohannad/countrycode/service/CountryService.java
@@ -3,11 +3,10 @@ package com.mohannad.countrycode.service;
 
 import com.mohannad.countrycode.model.CountryEntity;
 
+import java.util.List;
+
 /**
  * created by mohannad  on 25/10/19
  */
 public interface CountryService {
-
-    CountryEntity getCountry(String code);
-
-}
+    CountryEntity getCountry(String code);}
--- src/main/java/com/mohannad/countrycode/service/CountryServiceImpl.java
+++ src/main/java/com/mohannad/countrycode/service/CountryServiceImpl.java
@@ -1,8 +1,12 @@
 package com.mohannad.countrycode.service;
 
 import com.mohannad.countrycode.model.CountryEntity;
+import com.mohannad.countrycode.model.CountrylanguageEntity;
+import com.mohannad.countrycode.repository.CountryLanguageRepository;
 import com.mohannad.countrycode.repository.CountryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * created by mohannad  on 25/10/19
@@ -16,8 +20,10 @@ public class CountryServiceImpl implements CountryService {
         this.countryRepository = countryRepository;
     }
 
+
     @Override
     public CountryEntity getCountry(String code) {
         return countryRepository.findByCode(code);
     }
+
 }
