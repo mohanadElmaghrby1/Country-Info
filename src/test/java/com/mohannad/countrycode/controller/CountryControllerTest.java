e53bbed59bfbdeac20993b81337356a9bdaf796d
mohannad <mohanad2021996@gmail.com>
2019-10-26 09:40:54 +0200

    Testing CountryController

---
+++ src/test/java/com/mohannad/countrycode/controller/CountryControllerTest.java
@@ -0,0 +1,58 @@
+package com.mohannad.countrycode.controller;
+
+import com.mohannad.countrycode.model.CountryEntity;
+import com.mohannad.countrycode.model.CountryInfo;
+import com.mohannad.countrycode.service.CountryLanguageService;
+import com.mohannad.countrycode.service.CountryService;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class CountryControllerTest {
+
+
+    CountryController countryController;
+    @Mock
+    CountryLanguageService countryLanguageService;
+
+    @Mock
+    CountryService countryService;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        countryController =new CountryController(countryService,countryLanguageService);
+
+    }
+
+    @Test
+    void testCountryInfo() {
+        CountryEntity countryEntity =new CountryEntity();
+        countryEntity.setCode("ABC");
+        countryEntity.setName("ABCcountry");
+        when(countryService.getCountry(any())).thenReturn(countryEntity);
+        CountryInfo afgCountry = countryController.getCountryInfo("AFG");
+        assertNotNull(afgCountry);
+    }
+
+
+    @Test
+    void tesNotExistentCountryInfo() {
+        when(countryService.getCountry(any())).thenThrow(new EntityNotFoundException());
+        Executable executable = () -> countryController.getCountryInfo("adadaf");
+        assertThrows(EntityNotFoundException.class , executable);
+    }
+
+}
